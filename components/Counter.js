2ed7356252754130858da058ab5617e2cbe382ee
Stuhlmuller Monica <95758678+MonicaSth@users.noreply.github.com>
2022-12-08 14:09:54 +0200

    Add files via upload

---
+++ components/Counter.js
@@ -0,0 +1,35 @@
+import { useSelector, useDispatch } from "react-redux";
+import { couterActions } from "../Store/counter";
+import classes from "./Counter.module.css";
+
+const Counter = () => {
+  const dispatch = useDispatch();
+  const counter = useSelector((state) => state.counter.counter);
+  const show = useSelector((state) => state.counter.showCounter);
+
+  const incrementHandler = () => {
+    dispatch(couterActions.increment());
+  };
+
+  const decrementHandler = () => {
+    dispatch(couterActions.decrement());
+  };
+
+  const toggleCounterHandler = () => {
+    dispatch(couterActions.toggleCounter());
+  };
+
+  return (
+    <main className={classes.counter}>
+      <h1>Redux Counter</h1>
+      {show && <div className={classes.value}>{counter}</div>}
+      <div>
+        <button onClick={incrementHandler}>Increment</button>
+        <button onClick={decrementHandler}>Decrement</button>
+      </div>
+      <button onClick={toggleCounterHandler}>Toggle Counter</button>
+    </main>
+  );
+};
+
+export default Counter;
